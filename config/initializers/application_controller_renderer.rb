40ac7fd85266ca7c68d6313cc13b0ba4c9c35ae4
iidaharuku <iida.haruku.infratop@gmail.com>
2022-05-31 05:11:29 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0a57ae4423d2bae7a5988f41ab60f1b00a39f849f79d16ed69bd628813b0654c085490f31689a4fb962f64c21925f2835b60bba7da1d407189667f771f21ba19'
